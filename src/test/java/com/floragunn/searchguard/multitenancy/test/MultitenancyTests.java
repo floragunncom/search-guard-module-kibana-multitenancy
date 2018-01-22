37e3175ce7f4b5325554351757a6e9680ccaa28e
floragunn <dev@search-guard.com>
2018-01-22 17:03:25 +0100

    add additional tests

--- src/test/java/com/floragunn/searchguard/multitenancy/test/MultitenancyTests.java
+++ src/test/java/com/floragunn/searchguard/multitenancy/test/MultitenancyTests.java
@@ -1,26 +1,35 @@
 /*
  * Copyright 2017 by floragunn GmbH - All rights reserved
- * 
+ *
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed here is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * 
+ *
  * This software is free of charge for non-commercial and academic use.
  * For commercial use in a production environment you have to obtain a license
  * from https://floragunn.com
- * 
+ *
  */
 
 package com.floragunn.searchguard.multitenancy.test;
 
 import org.apache.http.HttpStatus;
 import org.apache.http.message.BasicHeader;
+import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest;
+import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest.AliasActions;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy;
+import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.floragunn.searchguard.support.ConfigConstants;
 import com.floragunn.searchguard.support.WildcardMatcher;
+import com.floragunn.searchguard.test.DynamicSgConfig;
 import com.floragunn.searchguard.test.SingleClusterTest;
 import com.floragunn.searchguard.test.helper.rest.RestHelper;
 import com.floragunn.searchguard.test.helper.rest.RestHelper.HttpResponse;
@@ -31,7 +40,162 @@ public class MultitenancyTests extends SingleClusterTest {
     protected String getResourceFolder() {
         return "multitenancy";
     }
-    
+
+    @Test
+    public void testNoDnfof() throws Exception {
+
+        final Settings settings = Settings.builder()
+                .put(ConfigConstants.SEARCHGUARD_ROLES_MAPPING_RESOLUTION, "BOTH")
+                .build();
+
+        setup(Settings.EMPTY, new DynamicSgConfig().setSgConfig("sg_config_nodnfof.yml"), settings);
+        final RestHelper rh = nonSslRestHelper();
+
+            try (TransportClient tc = getInternalTransportClient()) {
+                tc.admin().indices().create(new CreateIndexRequest("copysf")).actionGet();
+
+                tc.index(new IndexRequest("indexa").type("doc").id("0").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":\"indexa\"}", XContentType.JSON)).actionGet();
+                tc.index(new IndexRequest("indexb").type("doc").id("0").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":\"indexb\"}", XContentType.JSON)).actionGet();
+
+
+                tc.index(new IndexRequest("vulcangov").type("kolinahr").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+                tc.index(new IndexRequest("starfleet").type("ships").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+                tc.index(new IndexRequest("starfleet_academy").type("students").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+                tc.index(new IndexRequest("starfleet_library").type("public").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+                tc.index(new IndexRequest("klingonempire").type("ships").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+                tc.index(new IndexRequest("public").type("legends").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+
+                tc.index(new IndexRequest("spock").type("type01").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+                tc.index(new IndexRequest("kirk").type("type01").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+                tc.index(new IndexRequest("role01_role02").type("type01").setRefreshPolicy(RefreshPolicy.IMMEDIATE).source("{\"content\":1}", XContentType.JSON)).actionGet();
+
+                tc.admin().indices().aliases(new IndicesAliasesRequest().addAliasAction(AliasActions.add().indices("starfleet","starfleet_academy","starfleet_library").alias("sf"))).actionGet();
+                tc.admin().indices().aliases(new IndicesAliasesRequest().addAliasAction(AliasActions.add().indices("klingonempire","vulcangov").alias("nonsf"))).actionGet();
+                tc.admin().indices().aliases(new IndicesAliasesRequest().addAliasAction(AliasActions.add().indices("public").alias("unrestricted"))).actionGet();
+
+            }
+
+            HttpResponse resc;
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("indexa,indexb/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("indexa,indexb/_search?pretty", encodeBasicHeader("user_b", "user_b"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            String msearchBody =
+                    "{\"index\":\"indexa\", \"type\":\"doc\", \"ignore_unavailable\": true}"+System.lineSeparator()+
+                    "{\"size\":10, \"query\":{\"bool\":{\"must\":{\"match_all\":{}}}}}"+System.lineSeparator()+
+                    "{\"index\":\"indexb\", \"type\":\"doc\", \"ignore_unavailable\": true}"+System.lineSeparator()+
+                    "{\"size\":10, \"query\":{\"bool\":{\"must\":{\"match_all\":{}}}}}"+System.lineSeparator();
+            System.out.println("#### msearch a");
+            resc = rh.executePostRequest("_msearch?pretty", msearchBody, encodeBasicHeader("user_a", "user_a"));
+            Assert.assertEquals(200, resc.getStatusCode());
+            System.out.println(resc.getBody());
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("indexa"));
+            Assert.assertFalse(resc.getBody(), resc.getBody().contains("indexb"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("exception"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("permission"));
+
+            System.out.println("#### msearch b");
+            resc = rh.executePostRequest("_msearch?pretty", msearchBody, encodeBasicHeader("user_b", "user_b"));
+            Assert.assertEquals(200, resc.getStatusCode());
+            System.out.println(resc.getBody());
+            Assert.assertFalse(resc.getBody(), resc.getBody().contains("indexa"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("indexb"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("exception"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("permission"));
+
+            msearchBody =
+                    "{\"index\":\"indexc\", \"type\":\"doc\", \"ignore_unavailable\": true}"+System.lineSeparator()+
+                    "{\"size\":10, \"query\":{\"bool\":{\"must\":{\"match_all\":{}}}}}"+System.lineSeparator()+
+                    "{\"index\":\"indexd\", \"type\":\"doc\", \"ignore_unavailable\": true}"+System.lineSeparator()+
+                    "{\"size\":10, \"query\":{\"bool\":{\"must\":{\"match_all\":{}}}}}"+System.lineSeparator();
+
+            System.out.println("#### msearch b2");
+            resc = rh.executePostRequest("_msearch?pretty", msearchBody, encodeBasicHeader("user_b", "user_b"));
+            System.out.println(resc.getBody());
+            Assert.assertEquals(200, resc.getStatusCode());
+            Assert.assertFalse(resc.getBody(), resc.getBody().contains("indexc"));
+            Assert.assertFalse(resc.getBody(), resc.getBody().contains("indexd"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("exception"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("permission"));
+            int count = resc.getBody().split("\"status\" : 403").length;
+            Assert.assertEquals(3, count);
+
+            String mgetBody = "{"+
+                    "\"docs\" : ["+
+                        "{"+
+                             "\"_index\" : \"indexa\","+
+                            "\"_type\" : \"doc\","+
+                            "\"_id\" : \"0\""+
+                       " },"+
+                       " {"+
+                           "\"_index\" : \"indexb\","+
+                           " \"_type\" : \"doc\","+
+                           " \"_id\" : \"0\""+
+                        "}"+
+                    "]"+
+                "}";
+
+            resc = rh.executePostRequest("_mget?pretty",  mgetBody, encodeBasicHeader("user_b", "user_b"));
+            Assert.assertEquals(200, resc.getStatusCode());
+            Assert.assertFalse(resc.getBody(), resc.getBody().contains("\"content\" : \"indexa\""));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("indexb"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("exception"));
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("permission"));
+
+            mgetBody = "{"+
+                    "\"docs\" : ["+
+                        "{"+
+                             "\"_index\" : \"indexx\","+
+                            "\"_type\" : \"doc\","+
+                            "\"_id\" : \"0\""+
+                       " },"+
+                       " {"+
+                           "\"_index\" : \"indexy\","+
+                           " \"_type\" : \"doc\","+
+                           " \"_id\" : \"0\""+
+                        "}"+
+                    "]"+
+                "}";
+
+            resc = rh.executePostRequest("_mget?pretty",  mgetBody, encodeBasicHeader("user_b", "user_b"));
+            Assert.assertEquals(200, resc.getStatusCode());
+            Assert.assertTrue(resc.getBody(), resc.getBody().contains("exception"));
+            count = resc.getBody().split("root_cause").length;
+            Assert.assertEquals(3, count);
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("index*/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+
+
+            Assert.assertEquals(HttpStatus.SC_OK, (resc=rh.executeGetRequest("indexa/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("indexb/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("*/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("_all/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("notexists/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            Assert.assertEquals(HttpStatus.SC_NOT_FOUND, (resc=rh.executeGetRequest("indexanbh,indexabb*/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("starfleet/_search?pretty", encodeBasicHeader("user_a", "user_a"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+            Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (resc=rh.executeGetRequest("starfleet/_search?pretty", encodeBasicHeader("worf", "worf"))).getStatusCode());
+            System.out.println(resc.getBody());
+
+    }
+
     @Test
     public void testMt() throws Exception {
         final Settings settings = Settings.builder()
@@ -42,7 +206,7 @@ public class MultitenancyTests extends SingleClusterTest {
         HttpResponse res;
         String body = "{\"buildNum\": 15460, \"defaultIndex\": \"humanresources\", \"tenant\": \"human_resources\"}";
         Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (res = rh.executePutRequest(".kibana/config/5.6.0?pretty",body, new BasicHeader("sgtenant", "blafasel"), encodeBasicHeader("hr_employee", "hr_employee"))).getStatusCode());
-        
+
         body = "{\"buildNum\": 15460, \"defaultIndex\": \"humanresources\", \"tenant\": \"human_resources\"}";
         Assert.assertEquals(HttpStatus.SC_FORBIDDEN, (res = rh.executePutRequest(".kibana/config/5.6.0?pretty",body, new BasicHeader("sgtenant", "business_intelligence"), encodeBasicHeader("hr_employee", "hr_employee"))).getStatusCode());
 
@@ -50,11 +214,11 @@ public class MultitenancyTests extends SingleClusterTest {
         Assert.assertEquals(HttpStatus.SC_CREATED, (res = rh.executePutRequest(".kibana/config/5.6.0?pretty",body, new BasicHeader("sgtenant", "human_resources"), encodeBasicHeader("hr_employee", "hr_employee"))).getStatusCode());
         System.out.println(res.getBody());
         Assert.assertTrue(WildcardMatcher.match("*.kibana_*_humanresources*", res.getBody()));
-        
+
         Assert.assertEquals(HttpStatus.SC_OK, (res = rh.executeGetRequest(".kibana/config/5.6.0?pretty",new BasicHeader("sgtenant", "human_resources"), encodeBasicHeader("hr_employee", "hr_employee"))).getStatusCode());
         System.out.println(res.getBody());
         Assert.assertTrue(WildcardMatcher.match("*human_resources*", res.getBody()));
-        
+
     }
 
 }

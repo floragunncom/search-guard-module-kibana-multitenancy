3dd4d87242f01e50000bd1e23bf46b35f2d84be5
floragunn <dev@search-guard.com>
2018-01-22 21:21:31 +0100

    Test ITT-1132

---
+++ src/test/java/com/floragunn/searchguard/dlic/auditlog/ComplianceAuditlogTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016-2017 by floragunn GmbH - All rights reserved
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed here is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * This software is free of charge for non-commercial and academic use.
+ * For commercial use in a production environment you have to obtain a license
+ * from https://floragunn.com
+ *
+ */
+
+package com.floragunn.searchguard.dlic.auditlog;
+
+import org.apache.http.Header;
+import org.apache.http.HttpStatus;
+import org.elasticsearch.common.settings.Settings;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.floragunn.searchguard.support.ConfigConstants;
+import com.floragunn.searchguard.test.helper.rest.RestHelper.HttpResponse;
+
+public class ComplianceAuditlogTest extends AbstractAuditlogiUnitTest {
+
+    @Test
+    public void testSourceFilter() throws Exception {
+
+        Settings additionalSettings = Settings.builder()
+                .put("searchguard.audit.type", TestAuditlogImpl.class.getName())
+                .put(ConfigConstants.SEARCHGUARD_AUDIT_ENABLE_TRANSPORT, true)
+                .put(ConfigConstants.SEARCHGUARD_AUDIT_RESOLVE_BULK_REQUESTS, true)
+                .put(ConfigConstants.SEARCHGUARD_COMPLIANCE_HISTORY_EXTERNAL_CONFIG_ENABLED, false)
+                //.put(ConfigConstants.SEARCHGUARD_COMPLIANCE_HISTORY_WRITE_WATCHED_INDICES, "emp")
+                .put(ConfigConstants.SEARCHGUARD_COMPLIANCE_HISTORY_READ_WATCHED_FIELDS, "emp")
+                .put(ConfigConstants.SEARCHGUARD_AUDIT_CONFIG_DISABLED_TRANSPORT_CATEGORIES, "authenticated,GRANTED_PRIVILEGES")
+                .put(ConfigConstants.SEARCHGUARD_AUDIT_CONFIG_DISABLED_REST_CATEGORIES, "authenticated,GRANTED_PRIVILEGES")
+                .put("searchguard.audit.threadpool.size", 0)
+                .build();
+
+        setup(additionalSettings);
+        final boolean sendHTTPClientCertificate = rh.sendHTTPClientCertificate;
+        final String keystore = rh.keystore;
+        rh.sendHTTPClientCertificate = true;
+        rh.keystore = "auditlog/kirk-keystore.jks";
+        rh.executePutRequest("emp/doc/0?refresh", "{\"Designation\" : \"CEO\", \"Gender\" : \"female\", \"Salary\" : 100}", new Header[0]);
+        rh.executePutRequest("emp/doc/1?refresh", "{\"Designation\" : \"IT\", \"Gender\" : \"male\", \"Salary\" : 200}", new Header[0]);
+        rh.executePutRequest("emp/doc/2?refresh", "{\"Designation\" : \"IT\", \"Gender\" : \"female\", \"Salary\" : 300}", new Header[0]);
+        rh.sendHTTPClientCertificate = sendHTTPClientCertificate;
+        rh.keystore = keystore;
+
+        System.out.println("#### test source includes");
+        String search = "{" +
+                "   \"_source\":[" +
+                "      \"Gender\""+
+                "   ]," +
+                "   \"from\":0," +
+                "   \"size\":3," +
+                "   \"query\":{" +
+                "      \"term\":{" +
+                "         \"Salary\": 300" +
+                "      }" +
+                "   }" +
+                "}";
+        HttpResponse response = rh.executePostRequest("_search?pretty", search, encodeBasicHeader("admin", "admin"));
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
+        System.out.println(response.getBody());
+        Thread.sleep(1500);
+        System.out.println(TestAuditlogImpl.sb.toString());
+        Assert.assertEquals(1, TestAuditlogImpl.messages.size());
+        Assert.assertTrue(TestAuditlogImpl.sb.toString().contains("COMPLIANCE_DOC_READ"));
+        Assert.assertFalse(TestAuditlogImpl.sb.toString().contains("Designation"));
+        Assert.assertFalse(TestAuditlogImpl.sb.toString().contains("Salary"));
+        Assert.assertTrue(TestAuditlogImpl.sb.toString().contains("Gender"));
+    }
+
+
+}

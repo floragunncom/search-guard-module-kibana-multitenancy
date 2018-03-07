4b32d1ef8448b25692a518285ec2fefc20d0c480
Nils Bandener <git@bndnr.de>
2018-03-07 10:37:01 +0100

    Unit test for client cert auth of KeySetRetriever. Not finished yet, actual assertion that the server receives the client cert is missing.

--- src/test/java/com/floragunn/dlic/auth/http/jwt/keybyoidc/KeySetRetrieverTest.java
+++ src/test/java/com/floragunn/dlic/auth/http/jwt/keybyoidc/KeySetRetrieverTest.java
@@ -1,9 +1,26 @@
 package com.floragunn.dlic.auth.http.jwt.keybyoidc;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.security.KeyStore;
+import java.util.Map;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.ssl.PrivateKeyDetails;
+import org.apache.http.ssl.PrivateKeyStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.floragunn.searchguard.test.helper.file.FileHelper;
 
 public class KeySetRetrieverTest {
 	protected static MockIpdServer mockIdpServer;
@@ -36,5 +53,64 @@ public class KeySetRetrieverTest {
 		keySetRetriever.get();
 		Assert.assertEquals(1, keySetRetriever.getOidcCacheMisses());
 		Assert.assertEquals(1, keySetRetriever.getOidcCacheHits());
+	}
+
+	@Test
+	public void clientCertTest() throws Exception {
+
+		try (MockIpdServer sslMockIdpServer = new MockIpdServer(8085, true) {
+			@Override
+			protected void handleDiscoverRequest(HttpRequest request, HttpResponse response, HttpContext context)
+					throws HttpException, IOException {
+
+				System.out.println(request);
+
+				super.handleDiscoverRequest(request, response, context);
+
+				/*
+				 * X509Certificate[] certificateChain = (X509Certificate[])
+				 * request.getAttribute("javax.servlet.request.X509Certificate");
+				 * 
+				 * if (certificateChain == null || certificateChain.length == 0 ||
+				 * certificateChain[0] == null) {
+				 * requestContext.abortWith(buildForbiddenResponse("No certificate chain found!"
+				 * )); return; }
+				 * 
+				 * // The certificate of the client is always the first in the chain.
+				 * X509Certificate clientCert = certificateChain[0];
+				 */
+			}
+		}) {
+			SSLContextBuilder sslContextBuilder = SSLContexts.custom();
+
+			KeyStore trustStore = KeyStore.getInstance("JKS");
+			InputStream trustStream = new FileInputStream(
+					FileHelper.getAbsoluteFilePathFromClassPath("jwt/truststore.jks").toFile());
+			trustStore.load(trustStream, "changeit".toCharArray());
+
+			KeyStore keyStore = KeyStore.getInstance("JKS");
+			InputStream keyStream = new FileInputStream(
+					FileHelper.getAbsoluteFilePathFromClassPath("jwt/spock-keystore.jks").toFile());
+
+			keyStore.load(keyStream, "changeit".toCharArray());
+
+			sslContextBuilder.loadTrustMaterial(trustStore, null);
+
+			sslContextBuilder.loadKeyMaterial(keyStore, "changeit".toCharArray(), new PrivateKeyStrategy() {
+
+				@Override
+				public String chooseAlias(Map<String, PrivateKeyDetails> aliases, Socket socket) {
+					return "spock";
+				}
+			});
+
+			SettingsBasedSSLConfigurator.SSLConfig sslConfig = new SettingsBasedSSLConfigurator.SSLConfig(
+					sslContextBuilder.build(), new String[] { "TLSv1.2", "TLSv1.1" }, null, null);
+
+			KeySetRetriever keySetRetriever = new KeySetRetriever(sslMockIdpServer.getDiscoverUri(), sslConfig, false);
+
+			keySetRetriever.get();
+
+		}
 	}
 }
--- src/test/java/com/floragunn/dlic/auth/http/jwt/keybyoidc/MockIpdServer.java
+++ src/test/java/com/floragunn/dlic/auth/http/jwt/keybyoidc/MockIpdServer.java
@@ -3,7 +3,15 @@ package com.floragunn.dlic.auth.http.jwt.keybyoidc;
 import static com.floragunn.dlic.auth.http.jwt.keybyoidc.CxfTestTools.toJson;
 
 import java.io.Closeable;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
@@ -14,26 +22,34 @@ import org.apache.http.impl.bootstrap.ServerBootstrap;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpRequestHandler;
 
+import com.floragunn.searchguard.test.helper.file.FileHelper;
+
 class MockIpdServer implements Closeable {
 	final static String CTX_DISCOVER = "/discover";
 	final static String CTX_KEYS = "/api/oauth/keys";
 
-	private HttpServer httpServer;
-	private int port = 8081;
-	private String uri = "http://localhost:" + port;
+	private final HttpServer httpServer;
+	private final int port;
+	private final String uri;
+	private final boolean ssl;
 
 	MockIpdServer() throws IOException {
-		httpServer = ServerBootstrap.bootstrap().setListenerPort(port)
+		this(8081, false);
+	}
+
+	MockIpdServer(int port, boolean ssl) throws IOException {
+		this.port = port;
+		this.uri = (ssl ? "https" : "http") + "://localhost:" + port;
+		this.ssl = ssl;
+
+		this.httpServer = ServerBootstrap.bootstrap().setListenerPort(port).setSslContext(createSSLContext())
 				.registerHandler(CTX_DISCOVER, new HttpRequestHandler() {
 
 					@Override
 					public void handle(HttpRequest request, HttpResponse response, HttpContext context)
 							throws HttpException, IOException {
 
-						response.setStatusCode(200);
-						response.setHeader("Cache-Control", "public, max-age=31536000");
-						response.setEntity(new StringEntity("{\"jwks_uri\": \"" + uri + CTX_KEYS + "\",\n"
-								+ "\"issuer\": \"" + uri + "\", \"unknownPropertyToBeIgnored\": 42}"));
+						handleDiscoverRequest(request, response, context);
 
 					}
 				}).registerHandler(CTX_KEYS, new HttpRequestHandler() {
@@ -42,8 +58,7 @@ class MockIpdServer implements Closeable {
 					public void handle(HttpRequest request, HttpResponse response, HttpContext context)
 							throws HttpException, IOException {
 
-						response.setStatusCode(200);
-						response.setEntity(new StringEntity(toJson(TestJwks.ALL)));
+						handleKeysRequest(request, response, context);
 
 					}
 				})
@@ -72,6 +87,49 @@ class MockIpdServer implements Closeable {
 
 	public int getPort() {
 		return port;
+	}
+
+	protected void handleDiscoverRequest(HttpRequest request, HttpResponse response, HttpContext context)
+			throws HttpException, IOException {
+		response.setStatusCode(200);
+		response.setHeader("Cache-Control", "public, max-age=31536000");
+		response.setEntity(new StringEntity("{\"jwks_uri\": \"" + uri + CTX_KEYS + "\",\n" + "\"issuer\": \"" + uri
+				+ "\", \"unknownPropertyToBeIgnored\": 42}"));
+	}
+
+	protected void handleKeysRequest(HttpRequest request, HttpResponse response, HttpContext context)
+			throws HttpException, IOException {
+		response.setStatusCode(200);
+		response.setEntity(new StringEntity(toJson(TestJwks.ALL)));
+	}
+
+	private SSLContext createSSLContext() {
+		if (!this.ssl) {
+			return null;
+		}
+
+		try {
+			final TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+			final KeyStore trustStore = KeyStore.getInstance("JKS");
+			InputStream trustStream = new FileInputStream(
+					FileHelper.getAbsoluteFilePathFromClassPath("jwt/truststore.jks").toFile());
+			trustStore.load(trustStream, "changeit".toCharArray());
+			tmf.init(trustStore);
+
+			final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+			final KeyStore keyStore = KeyStore.getInstance("JKS");
+			InputStream keyStream = new FileInputStream(
+					FileHelper.getAbsoluteFilePathFromClassPath("jwt/node-0-keystore.jks").toFile());
+
+			keyStore.load(keyStream, "changeit".toCharArray());
+			kmf.init(keyStore, "changeit".toCharArray());
+
+			SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+			sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+			return sslContext;
+		} catch (final GeneralSecurityException | IOException exc) {
+			throw new RuntimeException(exc);
+		}
 	}
 
 }
